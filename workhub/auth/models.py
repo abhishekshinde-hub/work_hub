7d7405af46f92e4d3a82b3eff0a39f43888d6aac
TW Lenovo <abhishek.shinde@thoughtwin.com>
2026-08-14 12:46:22 +0530

    modles model indention error was resolved

--- workhub/auth/models.py
+++ workhub/auth/models.py
@@ -6,23 +6,23 @@ from sqlalchemy import Column, ForeignKey,Integer,String,Boolean,
 #     admin = "admin"
 #     manager = "manager"
 #     developer = "developer"
-    class User(Base) :
-        __tablename__ = 'users'   
-        user_id = Column(Integer,primary_key=True,index=True, autoincrement= True)
-        name = Column(String,nullable=False)
-        email_id = Column(String,unique=True,index= True,nullable = False)
-        username = Column(String,unique=True,index = True, nullable = False)
-        password = Column(String, nullable = False)
-        role = Column(String)
-        mobile_no = Column(String,unique=True, nullable = False)
-        gender = Column(String, nullable = False )
-        is_active = Column(Boolean,default=True,nullable=False)
-        refresh_token = relationship("Refreshtoken", back_populates= "user")
+class User(Base) :
+    __tablename__ = 'users'   
+    user_id = Column(Integer,primary_key=True,index=True, autoincrement= True)
+    name = Column(String,nullable=False)
+    email_id = Column(String,unique=True,index= True,nullable = False)
+    username = Column(String,unique=True,index = True, nullable = False)
+    password = Column(String, nullable = False)
+    role = Column(String)
+    mobile_no = Column(String,unique=True, nullable = False)
+    gender = Column(String, nullable = False )
+    is_active = Column(Boolean,default=True,nullable=False)
+    refresh_token = relationship("Refreshtoken", back_populates= "user")
 
-    class RefreshToken(Base) : 
-        __tablename__ = "refresh_token"
-        id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-        user_id = Column(Integer, ForeignKey('users.user_id'))
-        token = Column(String)
-        revoked = Column(Boolean, default=False)
-        user = relationship("User", back_populates='refresh_token')
+class RefreshToken(Base) : 
+    __tablename__ = "refresh_token"
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.user_id'))
+    token = Column(String)
+    revoked = Column(Boolean, default=False)
+    user = relationship("User", back_populates='refresh_token')
