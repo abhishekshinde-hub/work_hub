cb2cef106531b793ff75d439f1ba0db8c46dfa71
TW Lenovo <abhishek.shinde@thoughtwin.com>
2026-08-25 17:49:11 +0530

    refactor: move business logic from routers to service layer

---
+++ workhub/superadmin/router.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from workhub.auth.setting.auth_setting import verify_token
+from workhub.db_connection import get_db
+from workhub.superadmin import service
+
+router = APIRouter(tags=["superadmin"])
+
+
+@router.get("/superadmin/users")
+def get_all_normal_users(
+    db: Session = Depends(get_db), current_superadmin: str = Depends(verify_token)
+):
+    return service.service_get_all_normal_users(db,current_superadmin)
+
+@router.put("/users/{id}/role")
+def make_admin(
+    id: int,
+    db: Session = Depends(get_db) ,
+    current_superadmin: str = Depends(verify_token)
+):
+    return service.service_make_admin(id, db)
+
+@router.delete("/superadmin/users/{user_id}")
+def delete_normal_user(
+    user_id: int,
+    db: Session = Depends(get_db),
+    current_superadmin: dict = Depends(verify_token),
+):
+    return service.service_delete_normal_user(user_id,db,current_superadmin)
