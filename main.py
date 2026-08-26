7751468dd16ca3ee6cdb33f6f24e3a4cded44b2c
TW Lenovo <abhishek.shinde@thoughtwin.com>
2026-08-26 17:55:26 +0530

    import was fixed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 from fastapi import FastAPI
-from workhub.admin.routers import admin_power
+from workhub.admin import router
 from workhub.db_connection import Base,engine
 from workhub.auth import models
 from workhub.auth import router as auth_router
@@ -12,7 +12,7 @@ Base.metadata.create_all(bind=engine)
 "Include => When we make router then we have to run router files b'coz over main logic and code into the router thats why we include router in here so that when we run main over other file will be run"
 app.include_router(superadmin_router.router)
 app.include_router(auth_router.router)
-app.include_router(admin_power.router)
+app.include_router(router.router)
 app.include_router(user_router.router)
 
 
